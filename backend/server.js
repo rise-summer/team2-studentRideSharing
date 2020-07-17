e0f13b7c217501cdb91b85dedb39e79901305dc6
micheyale <michelle.chenwanjie@gmail.com>
2020-07-17 12:55:31 -0700

    Team2-0000 Updated Project structure files with backend folder and moved gitignore from frontend folder to root

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+// configure app
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+
+//Root Path
+app.get('/', (req, res) => {
+    res.send('<h1>API is running</h1>');
+});
+
+//Catch 404
+app.use((req, res, next) => {
+    res.status(404).send('<h2 align=center>Page Not Found</h2>');
+});
+
+//routes
+app.use('/api', routes);
+
+//find PORT in environment variables for server to run
+// if not found, use PORT = 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
