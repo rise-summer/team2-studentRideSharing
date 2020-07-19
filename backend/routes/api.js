bd1ceea45bc9c5e15c68c782ef2f2f71451b3462
EvelynL09 <lu.haiwei@yahoo.com>
2020-07-18 22:23:26 -0700

    Team2-0003 Set up proxy for using backend api
      - What:
        - Add setupProxy.js to frontend and proxy all /api url to backend server
        - Add the corresponding http-proxy-middleware to frontend/package.json
        - Make backend server send 200 with "Yeah!" as a simple test for /api
      - Testing:
        - run "npm install" in frontend dir
        - "npm start" the backend server
        - "npm start" the frontend and go to localhost:3000/api
          - The page should display "Yeah!".

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,7 +1,13 @@
-import express from 'express';
+// import express from 'express';
+//
+// const app = express();
 
-const app = express();
+let express = require('express');
+let router = express.Router();
 
+router.get('/', async function (req, res, next) {
+     res.status(200);
+     res.send("Yeah!");
+})
 
-
-module.exports = app;
+module.exports = router;
