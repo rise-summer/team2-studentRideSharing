11dcf05e98af5263613d9f45ed7d0bc3d0a590dd
EvelynL09 <lu.haiwei@yahoo.com>
2020-07-18 22:23:26 -0700

    Team2-0003 Set up proxy for using backend api
      - What:
        - Add setupProxy.js to frontend and proxy all /api url to backend server
        - Add the corresponding http-proxy-middleware to frontend/package.json
        - Make backend server send 200 with "Yeah!" as a simple test for /api
      - Testing:
        - run "npm install" in frontend dir
        - "npm start" the backend server
        - "npm start" the frontend and go to localhost:3000/api
          - The page should display "Yeah!".

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const app = express();
-
+var apiRouter = require('./routes/api');
 // configure app
 app.use(logger('dev'));
 app.use(cors());
@@ -16,16 +16,18 @@ app.get('/', (req, res) => {
     res.send('<h1>API is running</h1>');
 });
 
+//routes
+app.use('/api', apiRouter);
+
 //Catch 404
 app.use((req, res, next) => {
     res.status(404).send('<h2 align=center>Page Not Found</h2>');
 });
-
-//routes
-app.use('/api', routes);
 
 //find PORT in environment variables for server to run
 // if not found, use PORT = 5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// module.exports = app;
