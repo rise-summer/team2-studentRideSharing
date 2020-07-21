3dc0593b7bf4819f01c2dad23982d8f0e66912e7
EvelynL09 <lu.haiwei@yahoo.com>
2020-07-20 23:52:38 -0700

    Issue#6 Add api support for GET /api/rides/query?=querystring
      - What:
        - GET /api/rides/query?=querystring can fetch rides that
          - starts from a coordinates within queried distance from queried starting coordinates
          - within queried time frame (need further specification about the logic here)
        - Update for #5 to better incorporate with #6
        - Remove test.sh and use dbTest.js (previously dbInit.js) to perform current testing
    
      - Testing:
        - need "npm install querystring"
        - Ask me for apiKey.js and put it in backend/ folder
        - "npm start" the backend server
        - "npm start" the frontend
        - At /backend/, run "node ./test/dbTest.js"

--- backend/test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl -H "Content-Type: application/json" \
-  --data '{"pickUpAddr": [0, {"address": "69 Division Ave", "city": "Victorville", "state": "CA", "zip": 92392}], "destAddr":"x", "startDate": "new Date(2020, 6, 23, 13, 0)", "endDate": "new Date(2020, 6, 23, 14, 0) ", "price": 20.0, "capacity": 2, "car":  {"model": "Toyota", "make": "Camry", "color": "White", "plate": "7AVF369"}}' \
-  http://localhost:3000/api/rides/0/0
-  
-curl http://localhost:3000/api/rides?from=x&to=x&time=
