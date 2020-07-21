2cf340805363e7e146bec413029071a42420e510
EvelynL09 <lu.haiwei@yahoo.com>
2020-07-20 23:52:38 -0700

    Issue#6 Add api support for GET /api/rides/query?=querystring
      - What:
        - GET /api/rides/query?=querystring can fetch rides that
          - starts from a coordinates within queried distance from queried starting coordinates
          - within queried time frame (need further specification about the logic here)
        - Update for #5 to better incorporate with #6
        - Remove test.sh and use dbTest.js (previously dbInit.js) to perform current testing
    
      - Testing:
        - need "npm install querystring"
        - Ask me for apiKey.js and put it in backend/ folder
        - "npm start" the backend server
        - "npm start" the frontend
        - At /backend/, run "node ./test/dbTest.js"

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,36 +1,42 @@
 let express = require('express');
 let router = express.Router();
 let client = require('../db');
-// The database to use
-const dbName = "test";
+let querystring = require('querystring');
+
+//(temporary) Admin API for testing
+router.delete('/:collectionName', async function(req, res, next){
+  client.emptyCollection(req.params.collectionName);
+  res.status(200).send("Collection " + req.params.collectionName + " is empty.");
+})
 
 router.post('/rides/:userID/:rideID', async function(req, res, next){
   //TODO: validate login state - login user = given userid
   //TODO: validate input data - userID/rideID not existed in db, req body must include certain data
   const driverID = req.params.userID;
   const rideID = req.params.rideID;
-  const {pickUpAddr, destAddr, startDate, endDate, price, capacity, car} = req.body;
+  const {origin, destination, originCoords, destCoords, startDate, endDate, price, capacity, car} = req.body;
   let rideDocument = {
-    "driverID": driverID,
-    "rideID": rideID,
-    "pickUpAddr": pickUpAddr,
-    // "destAddr": [1, "UCLA"],
-    "destAddr": destAddr,
-    "startDate": startDate,//year, month (0 to 11), date, hours, minutes
+    "driverID": Number(driverID),
+    "rideID": Number(rideID),
+    "startLoc": origin,
+    "endLoc": destination,
+    "originCoords": originCoords,
+    "destCoords": destCoords,
+    "beginDate": startDate,//year, month (0 to 11), date, hours, minutes
     "endDate": endDate,
     "price": price,
     "capacity": capacity,
     "car": car
   }
-  const collection = client.dbCollection(dbName, 'Rides');
+  const collection = client.dbCollection('Rides');
   collection.insertOne(rideDocument, function(err, record){
     if(err) {//insert a record with an existing _id value
       console.log("New Ride Error");
       res.sendStatus(400);
     }
     else {
-      console.log("Record added as " + record);
-      res.sendStatus(201);//Created
+      console.log("Record added as " + JSON.stringify(record.ops[0]));
+      res.status(201).send("Record added as " + JSON.stringify(record.ops[0]));//Created
       //TODO: location header -> link to the generated ride
     }
   });
@@ -38,18 +44,33 @@ router.post('/rides/:userID/:rideID', async function(req, res, next){
 
 router.get('/rides', async function(req, res, next){
   //TODO: validate login state - login user = given userid
-  const pickUpAddr = req.query.from;
-  const destAddr = req.query.to;
-  //const date = req.query.time;
-  const date = new Date(2020, 6, 23, 13, 30);
-  console.log("get rides leave from " + pickUpAddr + " for " + destAddr);
-  const collection = client.dbCollection(dbName, 'Rides');
+  //TODO: search logic for begin and end date
+  const query = JSON.parse(req.query['query']);
+  console.log(query);
+  const originCoords = query['originCoords'];
+  const destCoords = query['destCoords'];
+  const beginDate = query['beginDate'];
+  const endDate = query['endDate'];
+  const distance = query['distance']; // within x miles
+  const collection = client.dbCollection('Rides');
+  const METERS_PER_MILE = 1609.34;
+
   collection.find({
-    startDate: {$lte: date},
-    endDate: {$gte: date}
+    beginDate: {$lte: beginDate},
+    endDate: {$gte: endDate},
+    originCoords:
+      {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: originCoords
+          },
+          $maxDistance: distance * METERS_PER_MILE
+        }
+      }
   }).toArray(function(err, rides){
     if(err) {
-      console.log("database find error for get /rides");
+      console.log(err);
       res.sendStatus(400);
     }
     // console.log();
