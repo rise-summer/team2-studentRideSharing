dd18e8e4bac148ccbf67651c25fe9deba5afe59f
micheyale <michelle.chenwanjie@gmail.com>
2020-08-14 17:45:29 -0700

    changed setupProxy to use netlify env var

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,16 +1,11 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
-const targetBE = 'http://localhost:5000';
-
-if(process.env.NODE_ENV === 'production') {
-    targetBE = 'https://t2-student-ride-sharing.herokuapp.com/';
-}
 
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target:  targetBE,
+            target:  process.env.TARGET_BE || 'http://localhost:5000',
             changeOrigin: true,
         })
     );
