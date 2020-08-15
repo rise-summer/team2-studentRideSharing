d7eb027434bfadb9bea36f04242abf8f9658c681
micheyale <michelle.chenwanjie@gmail.com>
2020-08-14 17:02:04 -0700

    add hosted BE URL to setupProxy and updated userID for Profile page

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,9 +1,16 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
+
+const targetBE = 'http://localhost:5000';
+
+if(process.env.NODE_ENV === 'production') {
+    targetBE = 'https://t2-student-ride-sharing.herokuapp.com/';
+}
+
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target:  targetBE,
             changeOrigin: true,
         })
     );
